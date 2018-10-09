b2d79af7f38dc53273a7449a3879ab60ee7328be
przekwas <luke@covalence.io>
2018-10-09 09:57:03 -0500

    updating env vars

--- src/server/config/production.ts
+++ src/server/config/production.ts
@@ -7,7 +7,7 @@ export default {
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         host: process.env.DB_HOST,
-        database: process.env.DB_DBNAME
+        database: process.env.DB_NAME
     },
     auth: {
         secret: process.env.SECRET
