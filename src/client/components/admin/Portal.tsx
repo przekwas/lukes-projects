c1c321c518beb41b978ea6976265ef7aaecb4e78
przekwas <luke@covalence.io>
2018-11-12 15:32:38 -0600

    moving rows to component and adjust style

--- src/client/components/admin/Portal.tsx
+++ src/client/components/admin/Portal.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import * as moment from 'moment';
-
 import json, { User } from '../../utils/api';
+
+import TableRowAdmin from './TableRowAdmin';
 
 export default class Portal extends React.Component<IPortalProps, IPortalState> {
     constructor(props: any) {
         super(props);
         this.state = {
             questions: [],
-            feedback: ''
         };
     }
 
@@ -26,41 +25,16 @@ export default class Portal extends React.Component<IPortalProps, IPortalState> 
         }
     }
 
-    async handleCheckboxToggle(e: React.ChangeEvent<HTMLInputElement>) {
-
-        let questions = this.state.questions;
-        let key = Number(e.target.name);
-        let id = questions[key].id;
-
-        if (questions[key].answered === 0) {
-            questions[key].answered = 1;
-        } else {
-            questions[key].answered = 0;
-        }
-
-        try {
-            let res = await json(`/api/questions/${id}`, 'PUT', {
-                id,
-                answered: questions[key].answered
-            });
-            this.setState({ questions });
-        } catch (e) {
-            this.setState({ feedback: 'Error saving questions, contact Luke!' });
-        }
-
-    }
-
     render() {
         return (
             <main className="py-5">
                 <div className="container py-5">
                     <div className="row">
                         <div className="col">
-                            <div className="d-flex justify-content-between align-items-center mb-1">
+                            <div className="mb-1">
                                 <h3 className="mb-2">Admin</h3>
-                                <span className="text-danger">{this.state.feedback}</span>
                             </div>
-                            <table className="table table-striped table-bordered shadow-lg">
+                            <table className="table table-striped table-hover table-bordered shadow-lg">
                                 <thead>
                                     <tr>
                                         <th scope="col">id</th>
@@ -72,22 +46,7 @@ export default class Portal extends React.Component<IPortalProps, IPortalState> 
 
                                     {this.state.questions.map((question, index) => {
                                         return (
-                                            <tr>
-                                                <th scope="row">{question.id}</th>
-                                                <td>{question.question}</td>
-                                                <td>
-                                                    <div className="form-check">
-                                                        <input
-                                                            name={index.toString()}
-                                                            className="form-check-input"
-                                                            type="checkbox"
-                                                            checked={question.answered === 0 ? false : true}
-                                                            onChange={(e) => this.handleCheckboxToggle(e)}
-                                                        />
-                                                        {question.answered === 0 ? <span className="text-danger">Unanswered</span> : <span className="text-success">Answered</span>}
-                                                    </div>
-                                                </td>
-                                            </tr>
+                                            <TableRowAdmin question={question} key={index} />
                                         );
                                     })}
 
@@ -103,7 +62,6 @@ export default class Portal extends React.Component<IPortalProps, IPortalState> 
 
 interface IPortalProps extends RouteComponentProps { }
 interface IPortalState {
-    feedback: string;
     questions: {
         id: number;
         question: string;
---
+++ src/client/components/admin/TableRowAdmin.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import json, { User } from '../../utils/api';
+
+export default class TableRowAdmin extends React.Component<ITableRowAdminProps, ITableRowAdminState> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            question: {
+                id: 0,
+                question: '',
+                answered: 0
+            }
+        };
+    }
+
+    componentWillMount() {
+        this.setState({ question: this.props.question })
+    }
+
+    async handleButtonClick(e: React.MouseEvent<HTMLButtonElement>) {
+
+        let question = this.state.question;
+        let id = question.id;
+        let answered = question.answered;
+
+        if (answered === 0) {
+            answered = 1;
+        } else {
+            answered = 0;
+        };
+
+        question.answered = answered;
+
+        try {
+            let res = await json(`/api/questions/${id}`, 'PUT', {
+                id,
+                answered
+            });
+
+            this.setState({ question });
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    render() {
+        const { id, question, answered } = this.state.question;
+        return (
+            <tr>
+                <th scope="row">{id}</th>
+                <td>{question}</td>
+                <td>
+                    <div className="btn-group">
+                        {
+                            answered === 0 ?
+                                <button onClick={(e) => this.handleButtonClick(e)} className="btn btn-sm btn-danger">Unanswered</button>
+                                :
+                                <button onClick={(e) => this.handleButtonClick(e)} className="btn btn-sm btn-success">Answered</button>
+                        }
+                    </div>
+                </td>
+            </tr>
+        );
+    }
+};
+
+interface ITableRowAdminProps {
+    question: {
+        id: number;
+        question: string;
+        answered: number;
+    };
+};
+
+interface ITableRowAdminState {
+    question: {
+        id: number;
+        question: string;
+        answered: number;
+    };
+};
