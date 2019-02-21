914e2be2da24ab0fa388118b6ba683fc47aea35d
przekwas <luke@covalence.io>
2019-02-21 09:33:06 -0600

    testing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const path = require('path');
 var nodeExternals = require('webpack-node-externals');
 
-var mode = process.env.NODE_ENV || 'development',
-
-const serverConfig = {
-  mode,
-  entry: './src/server/server.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-        options: {
-          configFile: 'tsconfig.server.json'
+const serverConfig = (argv, mode) => {
+  return {
+    mode: process.env.NODE_ENV || 'development',
+    entry: './src/server/server.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            configFile: 'tsconfig.server.json'
+          }
         }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  output: {
-    filename: 'server.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  target: 'node',
-  node: {
-    __dirname: false
-  },
-  externals: [nodeExternals()]
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      filename: 'server.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    target: 'node',
+    node: {
+      __dirname: false
+    },
+    externals: [nodeExternals()]
+  }
 }
 
 const clientConfig = {
