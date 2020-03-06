cc21370ed7fb62834bbcf792017d3e63dc7d10ff
przekwas <luke@covalence.io>
2020-03-06 16:21:57 -0600

    clean slate

--- src/client/utils/findRoute.ts
+++
@@ -1,7 +0,0 @@
-export const findRoute = (id: number) => {
-    if(id === 1) return '/Frontend/1'
-    else if (id === 2) return '/React/2'
-    else if (id === 3) return '/Node-Express/3'
-    else if (id === 4) return '/Database/4'
-    else if (id === 5) return '/Advanced/5'
-}
--- src/server/routes/api/categories.ts
+++
@@ -1,4 +0,0 @@
-import TableRouter from 'tablerouter';
-import { ICategories, Categories } from '../../db';
-
-export default new TableRouter<ICategories>(Categories, {}).Router
