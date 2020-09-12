e59fef014b65e40875d346b4c57b7e6e96774d17
przekwas <luke@covalence.io>
2020-09-12 14:20:23 -0500

    starting servert app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
 import express from 'express';
+import config from './config';
+
+async function startServer() {
+	const app = express();
+
+	app.listen(config.port, () => {
+		console.log(`
+        ######################################
+        ❤️  Server listening on port: ${config.port} ❤️ 
+        ######################################
+        `);
+	}).on('error', err => {
+		console.log(err);
+		process.exit(1);
+	});
+}
+
+startServer();
