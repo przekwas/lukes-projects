783ddac57c686f3ae398dbe21717ba576a911e72
przekwas <luke@covalence.io>
2020-09-12 15:30:15 -0500

    trying to add knex

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,7 +1,12 @@
 import expressLoader from './express';
+import knex from './knex';
 import logger from './logger';
 
 export default async function ({ expressApp }) {
 	await expressLoader({ app: expressApp });
 	logger.info('✌️ express loaded');
+
+	await knex.raw('select 1+1 as result');
+	logger.info('✌️ knex loaded');
+	
 }
