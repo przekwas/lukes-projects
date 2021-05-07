2b43ee38376e431387c80f5a0b6518d1acc9f5f2
przekwas <luke@covalence.io>
2021-05-07 15:54:35 -0500

    correcting type

--- src/services/users.ts
+++ src/services/users.ts
@@ -2,7 +2,7 @@ import { Query } from '../db';
 import type { UsersTable } from '../types/mysql';
 
 function find(col: string, val: string | number) {
-	return Query<UsersTable>('SELECT * FROM users WHERE ?? = ?', [col, val]);
+	return Query<UsersTable[]>('SELECT * FROM users WHERE ?? = ?', [col, val]);
 }
 
 export { find };
