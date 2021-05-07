b041eed63dd35bc31730af5d013165a552180390
przekwas <luke@covalence.io>
2021-05-07 16:01:23 -0500

    testing changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,6 @@
 import dotenv from 'dotenv';
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-const envFound = dotenv.config();
-
-if (envFound.error) {
-	throw new Error(`no .env file found`);
-}
+dotenv.config();
 
 export default {
 	port: parseInt(process.env.PORT),
