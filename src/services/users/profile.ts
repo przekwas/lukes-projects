14ea7d3b72b03e0ee48f0bc5f198d234d1549a86
przekwas <luke@covalence.io>
2021-06-04 22:29:18 -0500

    adjusting stored proc for profile

--- src/services/users/profile.ts
+++ src/services/users/profile.ts
@@ -2,14 +2,15 @@ import { Query } from '../../db';
 
 async function getProfile(userid: string) {
 	try {
-		const [[profile], [character_count]] = await Query<any>('CALL spGetProfileInfo(?)', [
-			userid
-		]);
+		const [[profile], [character_count], [location_count]] = await Query<any>(
+			'CALL spGetProfileInfo(?)',
+			[userid]
+		);
 		return {
 			profile: { ...profile },
 			counts: {
 				...character_count,
-				location_count: 0,
+				...location_count,
 				events_count: 0,
 				misc_notes_count: 0
 			}
