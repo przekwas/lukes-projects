688a487964e13aa61f88bab6b0e7aa9e2c911764
przekwas <luke@covalence.io>
2021-06-09 10:04:06 -0500

    changes

--- src/routes/api/users/profile.ts
+++ src/routes/api/users/profile.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { checkToken } from '../../../middlewares';
-import * as users from '../../../services/users/profile';
+import * as usersService from '../../../services/users/profile';
 
 const profileRouter = Router();
 
 profileRouter.get('/', checkToken, async (req, res, next) => {
 	try {
 		const userid = req.payload.id;
-		const profile = await users.getProfile(userid);
+		const profile = await usersService.getProfile(userid);
 		res.json(profile);
 	} catch (error) {
 		next(error);
