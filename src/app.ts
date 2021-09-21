062f8966440cade30d0e1493f96ebccb216c5d66
przekwas <luke@covalence.io>
2021-09-21 13:58:24 -0500

    fixing graphql

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,47 @@
 import express from 'express';
-import http from 'http';
 import config from './config';
 
-import { ApolloServer, gql } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {
+	hello: () => {
+		return 'suck it bitch';
+	}
+};
 
 async function startServer() {
 	const app = express();
-	const httpServer = http.createServer(app);
 
-	const server = new ApolloServer({
-		typeDefs: gql`
-			type Query {
-				hello: String
-			}
-		`,
-		resolvers: {
-			Query: {
-				hello: () => 'Suck it bitch'
-			}
-		},
-		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
-	});
-
-	await server.start();
-
-	server.applyMiddleware({ app });
+	app.use(
+		'/graphql',
+		graphqlHTTP({
+			schema: schema,
+			rootValue: root,
+			graphiql: true
+		})
+	);
 
 	(await import('./loaders')).default({ app });
 
-	httpServer
-		.listen(config.port, () => {
-			console.log(
-				`
+	app.listen(config.port, () => {
+		console.log(
+			`
             ###########################
             server running on port ${config.port}
             ###########################
             `
-			);
-		})
-		.on('error', error => {
-			console.log(error);
-			process.exit(1);
-		});
+		);
+	}).on('error', error => {
+		console.log(error);
+		process.exit(1);
+	});
 }
 
 startServer();
