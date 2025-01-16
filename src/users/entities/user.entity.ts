37b66ae96ed1cfc9ed870cf89c2e5642fbd1110f
Luke Przkwas <luke@covalence.io>
2025-01-15 21:13:20 -0600

    adding user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	CreateDateColumn,
+	UpdateDateColumn,
+	DeleteDateColumn
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column({ unique: true, length: 250 })
+	email: string;
+
+	@Column({ name: 'display_name', unique: true, length: 50 })
+	displayName: string;
+
+	@Column()
+	password: string;
+
+	@CreateDateColumn({ name: 'created_at' })
+	createdAt: Date;
+
+	@UpdateDateColumn({ name: 'updated_at' })
+	updatedAt: Date;
+
+	@DeleteDateColumn({ name: 'deleted_at', nullable: true })
+	deletedAt: Date | null;
+}
