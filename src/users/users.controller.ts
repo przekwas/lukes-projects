487e1e79dc99f14b25d8ca7c0bcab9d79c7de2c3
Luke Przkwas <luke@covalence.io>
2025-01-18 10:16:35 -0600

    adding pipe for uuid id param

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Get, Post, Param } from '@nestjs/common';
+import { Controller, Body, Get, Post, Param, ParseUUIDPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/user.dto';
 import { User } from './entities/user.entity';
@@ -8,7 +8,7 @@ export class UsersController {
 	constructor(private readonly userService: UsersService) {}
 
 	@Get(':id')
-	async getUser(@Param('id') id: string) {
+	async getUser(@Param('id', new ParseUUIDPipe()) id: string) {
 		return this.userService.findOneById(id);
 	}
 
