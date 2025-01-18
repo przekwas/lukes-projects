6ce1ffbe170a513978d5bbbc16175f5f6e06442a
Luke Przkwas <luke@covalence.io>
2025-01-18 14:01:39 -0600

    adding login dto to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post('login')
-	async login(@Body() { email, password }: { email: string; password: string }) {
-		return this.authService.login(email, password);
+	async login(@Body() dto: LoginDto) {
+		return this.authService.login(dto.email, dto.password);
 	}
 }
