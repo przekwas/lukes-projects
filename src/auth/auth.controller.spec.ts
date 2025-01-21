b0a5e28cc122b8f5a2255c8ba232aa333f85a141
Luke Przkwas <luke@covalence.io>
2025-01-20 20:55:39 -0600

    finish auth tests for real for now

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/auth.dto';
+import { NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 describe('AuthController', () => {
 	let authController: AuthController;
@@ -13,9 +15,65 @@ describe('AuthController', () => {
 		}).compile();
 
 		authController = module.get<AuthController>(AuthController);
+		authService = module.get<AuthService>(AuthService);
 	});
 
 	it('should be defined', () => {
 		expect(authController).toBeDefined();
+	});
+
+	describe('POST /auth/login > login()', () => {
+		it('should log a user in with valid credentials', async () => {
+			const mockLoginDto: LoginDto = {
+				email: 'test@test.com',
+				password: 'password'
+			};
+
+			const mockResult = { message: 'Login Success Yo!' };
+
+			(authService.login as jest.Mock).mockResolvedValue(mockResult);
+
+			const result = await authController.login(mockLoginDto);
+
+			expect(authService.login).toHaveBeenCalledWith(
+				mockLoginDto.email,
+				mockLoginDto.password
+			);
+			expect(result).toEqual(mockResult);
+		});
+
+		it('should throw UnauthorizedException if credentials are wrong', async () => {
+			const mockLoginDto: LoginDto = {
+				email: 'wrong@example.com',
+				password: 'badpass'
+			};
+
+			(authService.login as jest.Mock).mockRejectedValue(
+				new UnauthorizedException('Invalid credentials')
+			);
+
+			await expect(authController.login(mockLoginDto)).rejects.toThrow(UnauthorizedException);
+			expect(authService.login).toHaveBeenCalledWith(
+				mockLoginDto.email,
+				mockLoginDto.password
+			);
+		});
+
+		it('should throw NotFoundException if user not found', async () => {
+			const mockLoginDto: LoginDto = {
+				email: 'doesnotexist@example.com',
+				password: 'somepass'
+			};
+
+			(authService.login as jest.Mock).mockRejectedValue(
+				new NotFoundException('Invalid credentials')
+			);
+
+			await expect(authController.login(mockLoginDto)).rejects.toThrow(NotFoundException);
+			expect(authService.login).toHaveBeenCalledWith(
+				mockLoginDto.email,
+				mockLoginDto.password
+			);
+		});
 	});
 });
