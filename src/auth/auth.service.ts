4af7bd44bacfb4e1e16b2f8acf5ae424ddf06669
Luke Przkwas <luke@covalence.io>
2025-01-20 21:12:28 -0600

    adding two-token auth flow to service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,20 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { UsersService } from '../users/users.service';
+import { RefreshToken } from './entities/refresh-token.entity';
+import * as crypto from 'crypto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-	constructor(private readonly usersService: UsersService) {}
+	constructor(
+		private readonly usersService: UsersService,
+		private readonly jwtService: JwtService,
+		@InjectRepository(RefreshToken)
+		private readonly tokenRepo: Repository<RefreshToken>
+	) {}
 
 	async login(email: string, password: string) {
 		const user = await this.usersService.findOneByEmail(email);
@@ -17,6 +27,24 @@ export class AuthService {
 			throw new UnauthorizedException('Invalid credentials');
 		}
 
-		return { message: 'Login Success Yo!' };
+		const payload = { sub: user.id, email: user.email };
+		const accessToken = this.jwtService.sign(payload);
+
+		const refreshTokenStr = crypto.randomBytes(32).toString('hex');
+
+		const expires = new Date();
+		expires.setDate(expires.getDate() + 30);
+
+		const refreshEntity = this.tokenRepo.create({
+			token: refreshTokenStr,
+			user,
+			expiresAt: expires
+		});
+		await this.tokenRepo.save(refreshEntity);
+
+		return {
+			accessToken,
+			refreshToken: refreshTokenStr
+		};
 	}
 }
