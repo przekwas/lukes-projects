2c9c8e1358e0f9de19a8ceffa07c17f416a66a2b
Luke Przkwas <luke@covalence.io>
2025-01-20 21:01:06 -0600

    adding token table entity

---
+++ src/auth/entities/refresh-token.entity.ts
@@ -0,0 +1,30 @@
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	ManyToOne,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn
+} from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+
+@Entity('refresh_tokens')
+export class RefreshToken {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column()
+	token: string;
+
+	@Column({ nullable: true })
+	expiresAt?: Date;
+
+	@ManyToOne(() => User, { onDelete: 'CASCADE' })
+	user: User;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+}
