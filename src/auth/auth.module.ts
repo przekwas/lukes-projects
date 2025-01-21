5287ea95d5bb6e857c07910748a548e6b0e1fce3
Luke Przkwas <luke@covalence.io>
2025-01-20 21:07:52 -0600

    adding jwt to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { RefreshToken } from './entities/refresh-token.entity';
 
 @Module({
-	imports: [UsersModule],
+	imports: [
+		TypeOrmModule.forFeature([RefreshToken]),
+		UsersModule,
+		JwtModule.register({
+			secret: 'someSecretKeyREPLACEFROM_ENV_PLZ',
+			signOptions: { expiresIn: '15m' }
+		})
+	],
 	controllers: [AuthController],
-	providers: [AuthService],
-	exports: [AuthService]
+	providers: [AuthService]
 })
 export class AuthModule {}
