3259d2b3a856d959457a4d144aa8747b7b33e681
Luke Przkwas <luke@covalence.io>
2025-01-22 01:49:19 -0600

    adding nest and passport strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor() {
+		super({
+			secretOrKey: process.env.JWT_SECRET || 'someSecretKeyREPLACEFROM_ENV_PLZ',
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: false
+		});
+	}
+
+	async validate(payload: any) {
+		return {
+			userId: payload.sub,
+			email: payload.email
+		};
+	}
+}
