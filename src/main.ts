07766259e2036f58b3a95c294120f852b0a541ca
Luke Przkwas <luke@covalence.io>
2025-01-22 01:39:33 -0600

    migrating everything to v11 cause i am dumb

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
 
 	app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-	await app.listen(3000);
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
