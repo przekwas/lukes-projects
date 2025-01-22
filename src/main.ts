67be77ef9ee9792ce0340b2ab2d859eb0b560427
Luke Przkwas <luke@covalence.io>
2025-01-22 15:20:17 -0600

    adding cookie parser to server and types

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
 	app.useGlobalPipes(new ValidationPipe());
 
 	app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+	app.use(cookieParser());
 
 	await app.listen(process.env.PORT ?? 3000);
 }
