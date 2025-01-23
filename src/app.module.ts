c7da72accff985de15e4e10279e5171c33c7e5bb
Luke Przkwas <luke@covalence.io>
2025-01-23 14:59:28 -0600

    loading config module on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { WorkoutTrackerModule } from './workout-tracker/workout-tracker.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
 	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
 		TypeOrmModule.forRoot({
 			type: 'postgres',
-			host: 'localhost',
-			port: 5432,
-			username: 'postgres',
-			password: 'password',
-			database: 'lukes_projects',
+			host: process.env.DB_HOST,
+			port: parseInt(process.env.DB_PORT as string),
+			username: process.env.DB_USER,
+			password: process.env.DB_PASS,
+			database: process.env.DB_NAME,
 			entities: [__dirname + '/**/*.entity{.ts,.js}'],
 			synchronize: true
 		}),
