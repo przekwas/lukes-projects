97812f1adeb42b51d5615726a01e4e86441d87b5
przekwas <przekwas@gmail.com>
2025-08-19 14:04:11 -0500

    adding handy helpers

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -2,7 +2,15 @@ import { cleanEnv, str, port, url } from 'envalid';
 
 // load and validate process.env
 export const env = cleanEnv(process.env, {
-	NODE_ENV: str({ choices: ['development', 'test', 'productions'], default: 'development' }),
+	NODE_ENV: str({ choices: ['development', 'test', 'production'], default: 'development' }),
 	PORT: port({ default: 3000 }),
-	COOKIE_SECRET: str()
+	COOKIE_SECRET: str(),
+	// TEMP until docker pg is ready
+	DATABASE_URL: str({
+		default: 'postgres://user:pass@localhost:5432/db',
+		devDefault: 'postgres://user:pass@localhost:5432/db'
+	})
 });
+
+export const isProd = env.NODE_ENV === 'production';
+export const isDev = env.NODE_ENV === 'development';
