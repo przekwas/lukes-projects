fd48629eb940cfcfb0a328da9eb608652d221ca5
przekwas <przekwas@gmail.com>
2025-08-22 22:27:50 -0500

    initial drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'drizzle-kit';
+
+export default {
+    schema: './packages/db/src/schema.ts',
+    out: './drizzle',
+    dialect: 'postgresql',
+    dbCredentials: { url: process.env.DATABASE_URL! }
+} satisfies Config;
