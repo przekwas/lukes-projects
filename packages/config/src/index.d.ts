f17ec4f66ac4148268ad2ff42217e282d216f0cf
przekwas <przekwas@gmail.com>
2025-08-24 21:22:43 -0500

    cleaning old build tests

--- packages/config/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const env: Readonly<{
-    NODE_ENV: "development" | "test" | "production";
-    PORT: number;
-    COOKIE_SECRET: string;
-    DATABASE_URL: string;
-} & import("envalid").CleanedEnvAccessors>;
-export declare const isProd: boolean;
-export declare const isDev: boolean;
-//# sourceMappingURL=index.d.ts.map
