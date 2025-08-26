4fb97e0a763b60359f6e2bd938038a7eb2598aeb
przekwas <przekwas@gmail.com>
2025-08-25 21:23:08 -0500

    adding auth module to app

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module.js';
+import { AuthModule } from './auth/auth.module.js';
 // TEMP test
 import { PokeModule } from './poke/poke.module.js';
 
 @Module({
-	imports: [HealthModule, PokeModule],
-	controllers: [],
-	providers: []
+	imports: [HealthModule, PokeModule, AuthModule]
 })
 export class AppModule {}
