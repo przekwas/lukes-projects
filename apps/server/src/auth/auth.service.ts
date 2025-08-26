406137a4abeb4148475379dafeb640fe0e09e405
przekwas <przekwas@gmail.com>
2025-08-25 21:15:17 -0500

    adding auth service

---
+++ apps/server/src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service.js';
+import { hashPassword, verifyPassword } from 'packages/auth/dist/password.js';
+
+@Injectable()
+export class AuthService {
+	constructor(private users: UsersService) {}
+
+	async register(email: string, password: string, displayName: string, appKey?: string) {
+		const exists = await this.users.findByEmail(email);
+		if (exists) throw new BadRequestException('Email already in use');
+
+		const passwordHash = await hashPassword(password);
+		const u = await this.users.createUser(email, passwordHash, displayName);
+
+		if (appKey) await this.users.ensureMembership(u.id, appKey, 'member');
+
+		// TODO: set cookie/jwt later
+		return { ok: true };
+	}
+
+	async login(email: string, password: string) {
+		const u = await this.users.findByEmail(email);
+		if (!u) throw new UnauthorizedException('Invalid credentials');
+
+		const ok = await verifyPassword(u.passwordHash, password);
+		if (!ok) throw new UnauthorizedException('Invalid credentials');
+
+		// TODO: set cookie/jwt later
+		return { ok: true };
+	}
+}
