e1a15a95871e8098157ef71c3d93e65aec659ff4
przekwas <przekwas@gmail.com>
2025-08-25 21:22:14 -0500

    adding auth module together

---
+++ apps/server/src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller.js';
+import { AuthService } from './auth.service.js';
+import { UsersModule } from '../users/users.module.js';
+
+@Module({
+	imports: [UsersModule],
+	controllers: [AuthController],
+	providers: [AuthService]
+})
+export class AuthModule {}
