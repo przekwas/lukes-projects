1540c92a4339922c9692affd7c3d4369c3c53b3c
przekwas <przekwas@gmail.com>
2025-08-26 21:25:51 -0500

    updating auth module with sessions

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
 import { UsersModule } from '../users/users.module.js';
+import { SessionModule } from './session.module.js';
 
 @Module({
-	imports: [UsersModule],
+	imports: [UsersModule, SessionModule],
 	controllers: [AuthController],
 	providers: [AuthService]
 })
