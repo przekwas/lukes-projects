7b35225c9e7add398a999573c178e092017785ee
przekwas <przekwas@gmail.com>
2025-08-26 21:43:55 -0500

    adding cookie tests for poke controller

--- apps/server/src/poke/poke.controller.ts
+++ apps/server/src/poke/poke.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { db } from '@lukes-projects/db';
+import { SessionGuard } from '../auth/session.guard.js';
+import { RoleLevelGuard } from '../auth/role-level.guard.js';
 
 @Controller('poke')
 export class PokeController {
+	// TEMP public api route test
 	@Get()
 	async list() {
 		const rows = await db.query.poke.findMany({
@@ -10,5 +13,19 @@ export class PokeController {
 			limit: 10
 		});
 		return rows;
+	}
+
+	// TEMP private api route for logged in user
+	@UseGuards(SessionGuard)
+	@Get('secure')
+	async secure(@Req() req: any) {
+		return { ok: true, user: req.user };
+	}
+
+	// TEMP private api route only for admin logged in user
+	@UseGuards(SessionGuard, RoleLevelGuard(80))
+	@Get('admin')
+	async admin(@Req() req: any) {
+		return { ok: true, user: req.user, message: 'Admin route works!' };
 	}
 }
