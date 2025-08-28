3e3972f94a302ec5b6c1493d3f9f39b4a00dd3f6
przekwas <przekwas@gmail.com>
2025-08-27 22:31:14 -0500

    adding csrf middleware to app globally

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,12 +1,17 @@
 import 'reflect-metadata';
 
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { HealthModule } from './health/health.module.js';
 import { AuthModule } from './auth/auth.module.js';
+import { CsrfMiddleware } from './security/csrf.middleware.js';
 // TEMP test
 import { PokeModule } from './poke/poke.module.js';
 
 @Module({
 	imports: [HealthModule, PokeModule, AuthModule]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(CsrfMiddleware).forRoutes('*');
+	}
+}
