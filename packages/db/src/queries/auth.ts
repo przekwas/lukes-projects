0deb15525d3123226d0c822c9f0e8353d27e6480
przekwas <przekwas@gmail.com>
2025-08-29 13:27:46 -0500

    starting auth util query for auth/me

--- packages/db/src/queries/auth.ts
+++ packages/db/src/queries/auth.ts
@@ -1,3 +1,7 @@
 import { and, eq, gt } from 'drizzle-orm';
 import { db } from '../client.js';
 import { users, sessions, userRoles, roles, apps } from '../schema.js';
+
+// export async function findUserBySessionToken(sessionToken: string) {
+//     const [s] = await db.select({ userId: sessions.userId, revoked: sessions.revokedAt, })
+// }
